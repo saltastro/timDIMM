a7e7636cf7cd21b8cca6351a2504e7efa653a5df
T. E. Pickering <tim@saao.ac.za>
2010-08-20 19:55:58 +0200

    add steps to make plots and images and break out into multiple lines

--- timdimm.sh
+++ timdimm.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
-while [ 1 ]; do ./ave_frames 50 \!center.fits; ./find_boxes.py center.fits; ./measure_seeing 10000; ./lx200_log.py; ./lx200_guide.py; mv centroids.dat data/centroids_`date -u '+%Y%m%d-%H%M%S'`.dat; done
+while [ 1 ]; do 
+    ./ave_frames 50 \!center.fits
+    ./find_boxes.py center.fits
+    ./measure_seeing 10000 
+    ./lx200_log.py
+    ./lx200_guide.py
+    mv centroids.dat data/centroids_`date -u '+%Y%m%d-%H%M%S'`.dat
+    ./plot.gnu
+    echo "image;text 25 5 # text={Seeing = `cat seeing.out`\"}" | xpaset timDIMM regions
+    echo "image;text 290 5 # text={R0 = `cat r0.out` cm}" | xpaset timDIMM regions
+    xpaset -p timDIMM saveimage png ds9.png
+    mv ds9.png /Users/timdimm/Sites/images/.
+done
