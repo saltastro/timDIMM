378e914c185b6facb0afb45622a0f9e1607448a1
T. E. Pickering <tim@saao.ac.za>
2011-01-18 13:11:27 +0200

    add -p to mkdir to force creation of directories; archive any FITS images that get created

--- rotate.sh
+++ rotate.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-mkdir ../timDIMM_data/$1
+mkdir -p ../timDIMM_data/$1
 rm -f centroids.dat
 gzip -v data/centroids_*.dat
 mv -f data/* ../timDIMM_data/$1
 mv -f lx200.log seeing.dat ../timDIMM_data/$1
+mv -f *.fits ../timDIMM_data/$1
