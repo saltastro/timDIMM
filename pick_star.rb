0f5efea0716df5edb9ab27039f17cdf15568c7c8
T. E. Pickering <tim@saao.ac.za>
2011-03-22 20:19:33 +0200

    add logic to avoid weather mast; now choosing stars based on mag and fall back to 2nd best in case the best is behind the mast; do spiral search and centering right after a slew

--- pick_star.rb
+++ pick_star.rb
@@ -9,27 +9,47 @@ sleep(2)
 s = GTO900.new('massdimm', 7001)
 t = Turbina.new
 
+best_hr = ""
+
 s.clear
 lst = s.lst
-s.clear
 airmass = s.airmass
+az = s.az
+alt = s.alt
 s.close
 sleep(1)
 
-if airmass < 1.5
+az = hms2deg(az)
+alt = hms2deg(alt)
+
+if airmass < 1.5 && !(alt < 75.0 && az > 285.0 && az < 295.0)
   puts "Fine to stay here."
   stat = t.status
   if stat =~ /READY/
     puts t.run
   end
 else 
-  best_hr = best_star(lst)
+
+  best_hrs = best_star(lst)
+  if best_hr == best_hrs[0]
+    best_hr = best_hrs[1]
+  else
+    best_hr = best_hrs[0]
+  end
+  
   puts "Should move. Best HR number is #{best_hr}"
   puts t.stop
   system("./gto900_hr.rb #{best_hr}")
   system("echo \"#{best_hr}\" > current_object")
   puts t.object(best_hr)
+  system("./gto900_offset.rb w")
+  t.background
+  system("./gto900_hr.rb #{best_hr}")
+  sleep(3)
+  system("./spiral_search_gto900.py")
+  system("./gto900_guide.rb")
   puts t.run
+
 end
 
 t.close
