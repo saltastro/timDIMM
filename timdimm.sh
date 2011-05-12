40aeda9012a576d041a427fd7392292450fc684b
T. E. Pickering <tim@saao.ac.za>
2011-05-12 15:36:47 +0200

    copy seeing data over so EDS can capture it for display

--- timdimm.sh
+++ timdimm.sh
@@ -13,6 +13,8 @@ while [ 1 ]; do
     echo "image;text 25 5 # text={Seeing = `cat seeing.out`\"}" | xpaset timDIMM regions
     echo "image;text 290 5 # text={R0 = `cat r0.out` cm}" | xpaset timDIMM regions
     scp seeing.out seeing.dat massdimm@massdimm:~/seeing/suthdimm/.
+    date +'%Y-%m-%dT%H:%M:%S%z' >> seeing.out
+    cp seeing.out ~/Sites/seeing.txt
     xpaset -p timDIMM saveimage png ds9.png
     mv ds9.png /Users/timdimm/Sites/images/.
 done
