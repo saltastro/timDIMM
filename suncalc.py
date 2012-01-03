8a9d6d95abf60e7081017f867335b41e5bf77f33
T. E. Pickering <tim@saao.ac.za>
2012-01-04 01:37:03 +0200

    add more details and print sun elevation

--- suncalc.py
+++ suncalc.py
@@ -12,12 +12,23 @@ salt.long = "20:48:30"
 salt.elevation = 1800
 salt.temp = 10
 salt.compute_pressure()
-
 salt.horizon = '-12'
-rise = ephem.localtime(salt.next_rising(ephem.Sun())).ctime()
-set = ephem.localtime(salt.next_setting(ephem.Sun())).ctime()
+sun = ephem.Sun()
 
-if sys.argv[1] == 'rise':
-    print rise
-else:
-    print set
+sun.compute(salt)
+print float(sun.alt)*180.0/np.pi
+
+rise = ephem.localtime(salt.next_rising(sun))
+set = ephem.localtime(salt.next_setting(sun))
+
+now = ephem.localtime(ephem.now())
+
+t2rise = rise-now
+t2set = set-now
+
+print "12 degree evening twilight ends in ", t2set
+print "12 degree morning twilight begins in ", t2rise
+
+print "Morning twilight: ", rise.ctime()
+
+print "Evening twilight: ", set.ctime()
