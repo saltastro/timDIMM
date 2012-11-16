760fa0e6df3d5d5855cfdd8843285b426859f9f3
T. E. Pickering <tim@saao.ac.za>
2012-11-16 17:56:41 +0200

    update script to make movie of all skycam images

--- mkmovie_all.sh
+++ mkmovie_all.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-x=1; for i in `ls *.JPG`; do counter=$(printf %03d $x); ln -sf "$i" img"$counter".jpg; x=$(($x+1)); done
-/opt/local/bin/ffmpeg -y -r 10 -sameq -f image2 -i img%03d.jpg latest_anim.webm
-/opt/local/bin/ffmpeg -y -r 10 -sameq -f image2 -i img%03d.jpg latest_anim.mp4
+x=1; for i in `ls *.jpg`; do counter=$(printf %03d $x); ln -sf "$i" img"$counter".jpg; x=$(($x+1)); done
+/opt/local/bin/ffmpeg -y -r 10 -sameq -f image2 -i img%03d.jpeg complete_anim.webm
+/opt/local/bin/ffmpeg -y -r 10 -sameq -f image2 -i img%03d.jpeg complete_anim.mp4
