9ece24d1d1803ec32e5ec74cddb900e0937d0518
T. E. Pickering <tim@saao.ac.za>
2012-11-19 13:27:09 +0200

    tweak pointing; fix movie script; fix db_insert

--- db_insert.py
+++ db_insert.py
@@ -10,7 +10,7 @@ fp = open(file, 'r')
 for l in fp.readlines():
     data = l.split()
     try:
-        cmd = "mysql --user=%s --password=%s -h sdb.salt \
+        cmd = "mysql5 --user=%s --password=%s -h sdb.salt \
         -D sdb -e \"INSERT INTO MassDimm \
         VALUES(\'%s %s\', NULL, %s, NULL, NULL, NULL, NULL, NULL, NULL);\"" % \
         (os.environ['MASSSQL'], os.environ['MASSPASS'], data[0], data[1], data[4])
