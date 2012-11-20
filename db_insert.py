7e836f57832b2794d744087a301ba556a6f74eef
T. E. Pickering <tim@saao.ac.za>
2012-11-20 23:35:01 +0200

    pep8 compliance

--- db_insert.py
+++ db_insert.py
@@ -13,9 +13,9 @@ for l in fp.readlines():
         cmd = "mysql5 --user=%s --password=%s -h sdb.salt \
         -D sdb -e \"INSERT INTO MassDimm \
         VALUES(\'%s %s\', NULL, %s, NULL, NULL, NULL, NULL, NULL, NULL);\"" % \
-        (os.environ['MASSSQL'], os.environ['MASSPASS'], data[0], data[1], data[4])
+        (os.environ['MASSSQL'], os.environ['MASSPASS'],
+         data[0], data[1], data[4])
         #print cmd
         os.system(cmd)
     except:
         pass
-    
