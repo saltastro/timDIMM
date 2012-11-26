f6f9597a06a3c906829ee390ea1ee03dc67c5963
T. E. Pickering <tim@saao.ac.za>
2012-11-26 10:00:21 +0300

    clear out all SAAO specific bits

--- mkmovie.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cd /Library/WebServer/Documents/skycam/Data
-x=1; for i in `ls *.jpg | tail -200`; do counter=$(printf %03d $x); ln -sf "$i" img"$counter".jpeg; x=$(($x+1)); done
-/opt/local/bin/ffmpeg -y -r 10 -sameq -f image2 -i img%03d.jpeg latest_anim.webm
-/opt/local/bin/ffmpeg -y -r 10 -sameq -f image2 -i img%03d.jpeg latest_anim.mp4
--- mkmovie_all.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-x=1; for i in `ls *.jpg`; do counter=$(printf %03d $x); ln -sf "$i" img"$counter".jpeg; x=$(($x+1)); done
-/opt/local/bin/ffmpeg -y -r 10 -sameq -f image2 -i img%03d.jpeg complete_anim.webm
-/opt/local/bin/ffmpeg -y -r 10 -sameq -f image2 -i img%03d.jpeg complete_anim.mp4
