24100cec232e4b19b9801d2cb25af72c25aad515
T. E. Pickering <tim@saao.ac.za>
2012-12-21 11:49:10 +0200

    configure partial-dir for rsync as per garith

--- rotate.sh
+++ rotate.sh
@@ -10,12 +10,12 @@ mv -f data/* ../timDIMM_data/$1
 ./db_insert.py seeing.dat
 mv -f *.log seeing.dat ../timDIMM_data/$1
 mv -f *.fits ../timDIMM_data/$1
-/usr/bin/rsync -av ../timDIMM_data/20* nfs4::seeingdata/massdimm/.
+/usr/bin/rsync -av --partial-dir=.rsync-partial/ ../timDIMM_data/20* nfs4::seeingdata/massdimm/.
 touch SYNCME
 
 # now handle MASS data
 ../masspipe/getmass.sh `date -v -12H +'%Y%m%d'`
-/usr/bin/rsync -av ../masspipe/data/20* nfs4::seeingdata/mass/.
+/usr/bin/rsync -av --partial-dir=.rsync-partial/ ../masspipe/data/20* nfs4::seeingdata/mass/.
 
 # now rotate skycam data
 mkdir -p /Library/WebServer/Documents/skycam/`date -v -1d +'%Y/%m%d'`/
@@ -23,5 +23,5 @@ cd /Library/WebServer/Documents/skycam/`date -v -1d +'%Y/%m%d'`/
 mv /Library/WebServer/Documents/skycam/Data/* .
 chmod go+r *
 cd /Library/WebServer/Documents/skycam
-/usr/bin/rsync -av 20* nfs4::seeingdata/skycam/.
+/usr/bin/rsync -av --partial-dir=.rsync-partial/ 20* nfs4::seeingdata/skycam/.
 
