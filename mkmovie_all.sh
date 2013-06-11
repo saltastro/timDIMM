eb0f817902af64f198778619e69e49e7b4025399
T. E. Pickering <tim@saao.ac.za>
2013-06-11 15:54:16 +0200

    fix up encoding the webm version to use the mp4 as a starting point.  can't tweak quality when encoding from the jpeg files for some reason

--- mkmovie_all.sh
+++ mkmovie_all.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
+cd /Library/WebServer/Documents/skycam/Data
 x=1; for i in `ls *.jpg`; do counter=$(printf %03d $x); ln -sf "$i" aimg"$counter".jpeg; x=$(($x+1)); done
-/opt/local/bin/ffmpeg -y -r 10 -g 120 -level 216 -profile 0 -qmax 42 -qmin 10 -rc_buf_aggressivity 0.95 -vb 2M -f image2 -i aimg%03d.jpeg complete_anim.webm
-/opt/local/bin/ffmpeg -y -r 10 -sameq -f image2 -i aimg%03d.jpeg complete_anim.mp4
+/opt/local/bin/ffmpeg -y -r 10 -q:v 0 -f image2 -i aimg%03d.jpeg complete_anim.mp4
+/opt/local/bin/ffmpeg -i complete_anim.mp4 -y -maxrate 2M -minrate 2M -b:v 2M complete_anim.webm
 rm aimg*.jpeg
