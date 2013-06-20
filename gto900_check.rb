8915de632f86092447f6cc0592c2a0913c8ca9cc
T. E. Pickering <tim@saao.ac.za>
2013-06-20 12:26:42 +0200

    print out time as reported by telescope

--- gto900_check.rb
+++ gto900_check.rb
@@ -19,5 +19,6 @@ ha = sexagesimal(calc_ha(15*hms2deg(lst), 15*hms2deg(r))/15.0)
 side = s.pier?
 
 puts "At RA = %s, Dec = %s, HA = %s, LST = %s, Alt = %s, Az = %s, secz = %.2f, on the %s side of the pier" % [r, d, ha, lst, alt, az, secz, side]
+puts "Time: %s" % s.get_local_time
 
 s.close
